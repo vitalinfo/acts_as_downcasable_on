25661f88ce8ae37cff6a426b53e3d81a29c23fd4
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-07-03 06:07:06 +0000

    Remove trace

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,4 +1,3 @@
-puts 'asdasdas'
 RSpec.shared_examples_for 'test instance attributes' do
   it do
     { value_downcasable_1: 'text_1', value_downcasable_2: 'text_2', value: 'TEXT' }.each do |attr, value|
